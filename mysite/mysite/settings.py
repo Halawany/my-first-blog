0c6fee575fc2f253e68d5fb0df42f61ce6e63df2
halawany <halawany.me@gmail.com>
2018-12-27 02:25:13 +0200

    from postgresql to sqlite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,13 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mysite',
-        'USER': 'postgres',
-        'PASSWORD': 'django123',
-        'HOST': 'localhost',
-        'PORT': 5432
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
